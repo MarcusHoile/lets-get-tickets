15371c76459b34d300230d9a2d200237f23bbc7c
MollyMoile <marcus.hoile@gmail.com>
2014-04-07 19:47:06 +1000

    converted date format

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,6 +2,10 @@ function selectThisEvent(selection) {
 	// grab the information from selected event
 	// and insert it into form inputs
 	var what = selection.find('.event-list-name').text();
+	var when = selection.find('.event-list-date').text();
+	var where = selection.find('.event-list-venue').text();
 
 	$('#event_what').val(what.trim());
+	$('#event_where').val(where.trim());
+	$('#event_event_when_text').val(when.trim());
 }
