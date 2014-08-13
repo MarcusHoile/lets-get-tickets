b30a6738a9ca260178eace2955cc36dd51c70f69
MollyMoile <marcus.hoile@gmail.com>
2014-08-13 23:48:33 +1000

    fuck off

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_filter :authenticate_user
+  # skip_before_filter :authenticate_user 
 	# all the static pages are here
   def aboutus
   end
