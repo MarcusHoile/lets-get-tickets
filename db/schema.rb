014058e1393efec342ad7996a1e3cb071c4a78be
MollyMoile <marcus.hoile@gmail.com>
2014-10-15 23:52:57 +1100

    planddit icon placeholder, migration image to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820122040) do
+ActiveRecord::Schema.define(version: 20141015124705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140820122040) do
     t.decimal  "lng"
     t.string   "status",      default: "open"
     t.boolean  "ticket",      default: false
+    t.string   "image"
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
