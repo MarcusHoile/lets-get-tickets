8d9659a6f5dfdb2867bd87159d9e35f12da3a77e
marcushoile <marcus.hoile@gmail.com>
2015-04-29 22:13:01 +1000

    migration add booked

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015124705) do
+ActiveRecord::Schema.define(version: 20150429105400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20141015124705) do
     t.string   "status",      default: "open"
     t.boolean  "ticket",      default: false
     t.string   "image"
+    t.boolean  "booked",      default: false
   end
 
   add_index "events", ["user_id"], name: "index_events_on_user_id", using: :btree
