055ea9bf7de9b288b2bcd9d4b393cfc34a08e522
marcushoile <marcus.hoile@gmail.com>
2015-04-29 22:13:22 +1000

    schema at top of invite model

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,3 +1,10 @@
+# rsvp           string           default: "Undecided"
+# user_id        integer
+# event_id       integer
+# payment        boolean        default: false
+# reason         string
+# payment_method string
+
 class Invite < ActiveRecord::Base
 
   belongs_to :event
