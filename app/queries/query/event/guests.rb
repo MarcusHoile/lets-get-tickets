d40b2b68fc994d4b879b767fa891357e6bbc811e
marcushoile <marcus.hoile@gmail.com>
2015-05-19 01:11:34 +1000

    query for guests, and status

---
+++ app/queries/query/event/guests.rb
@@ -0,0 +1,30 @@
+module Query
+  module Event
+    module Guests
+      extend self
+
+      def all(event)
+        event.guests
+         .extending(Scopes)
+      end
+
+      module Scopes
+        def confirmed
+          joins(:invites).where('invites.rsvp' => 'going').uniq
+        end
+
+        def not_going
+          joins(:invites).where('invites.rsvp' => 'not-going').uniq
+        end
+
+        def undecided
+          joins(:invites).where('invites.rsvp' => 'undecided').uniq
+        end
+
+        def maybe
+          joins(:invites).where('invites.rsvp' => 'maybe').uniq
+        end
+      end
+    end
+  end
+end
