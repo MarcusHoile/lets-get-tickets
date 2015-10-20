a210a0c5f259a8fc2f95f290a3edc4c2cadd2f89
marcushoile <marcus.hoile@gmail.com>
2015-10-20 19:08:03 +1100

    update demo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,19 +17,19 @@ open_plan =   Plan.create(what: "The Black Keys",
                             deadline: 2.weeks.from_now,
                             price: 90,
                             demo: true)
-# closed_plan = plan.create(what: "Sydney Swans vs West Coast Eagles",
-#                             when:  3.weeks.from_now,
-#                             where: "Sydney Cricket Ground",
-#                             user_id: host.id,
-#                             deadline: 1.day.ago,
-#                             price: 60,
-#                             booked: true,
-#                             demo: true)
+closed_plan = plan.create(what: "Sydney Swans vs West Coast Eagles",
+                            when:  3.weeks.from_now,
+                            where: "Sydney Cricket Ground",
+                            user_id: host.id,
+                            deadline: 1.day.ago,
+                            price: 60,
+                            booked: true,
+                            demo: true)
 
 rsvps = ['going', 'not-going', 'maybe', 'undecided']
 # create invites for plans
 open_plan.invites.create(user_id: host.id, rsvp: 'going')
-# closed_plan.invites.create(user_id: host.id, rsvp: 'going')
+closed_plan.invites.create(user_id: host.id, rsvp: 'going')
 
 open_plan.media.create!(url: 'http://a.dilcdn.com/bl/wp-content/uploads/sites/8/2012/04/TheBlackKeys.jpg', media_type: 'image')
 open_plan.media.create!(source_id: '0MhwaBVJghoZsEzHbr0qjl' , media_type: 'spotify')
@@ -37,7 +37,7 @@ open_plan.media.create!(url: 'https://www.youtube.com/watch?v=a_426RiwST8', medi
 
 guests.map do |guest|
   open_plan.invites.create(user_id: guest.id, rsvp: rsvps.sample)
-  # closed_plan.invites.create(user_id: guest.id, rsvp: rsvps.sample)
+  closed_plan.invites.create(user_id: guest.id, rsvp: rsvps.sample) 
 end
 
 # add media for plans, video music images
